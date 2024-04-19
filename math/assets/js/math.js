5e78e1eee966f6938b17c510cdcb39e100ecb587
Alexsandro Macedo <97038931+AlexxsandroM@users.noreply.github.com>
2024-04-19 14:00:27 -0300

    Introdução Math

---
+++ math/assets/js/math.js
@@ -0,0 +1,25 @@
+let numero = 43.50
+console.log(numero)
+
+
+console.log(`Math.ceil(${numero} \nArredondamento para cima = ${Math.ceil(numero)})`)
+
+console.log(`Math.floor(${numero} \nArredondamento para baixo = ${Math.floor(numero)})`)
+
+console.log(`Math.round(${numero} \nArredondamento = ${Math.round(numero)})`)
+
+console.log(`Math.max(34,67,89,45,12,47,98,441,15) Maior número: ${Math.max(34,67,89,45,12,47,98,441,15)}`)
+
+console.log(`Math.min(34,67,89,45,12,47,98,441,15) Menor número: ${Math.min(34,67,89,45,12,47,98,441,15)}`)
+
+console.log(`parseInt(numero) Número inteiro: ${parseInt(numero)}`)
+
+console.log(`Math.pow(8,3) Potência: ${Math.pow(8,3)}`)
+
+let numeroSorteado = Math.random()
+console.log(`Math.random(numero) Número aleatório: ${numeroSorteado}`)
+
+console.log(`Math.sqrt(25) Raiz Quadrada: ${Math.sqrt(25)}`)
+
+console.log(`Math.cbrt(27) Raiz Cubica: ${Math.cbrt(27)}`)
+
